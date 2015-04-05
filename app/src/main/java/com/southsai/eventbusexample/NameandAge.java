7522e2b1d3d8e583a2bbad347179536465c47193
Thao Han <hanvthao@bitbucket.org>
2015-04-05 13:33:31 +0700

    1st update

---
+++ app/src/main/java/com/southsai/eventbusexample/NameandAge.java
@@ -0,0 +1,22 @@
+package com.southsai.eventbusexample;
+
+/**
+ * Created by thao on 4/5/15.
+ */
+public class NameandAge {
+    private String name;
+    private int age;
+
+    public NameandAge(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
